04157fffd9b8b91c01b0ecc891c81f0346fef5ec
Jack <jackcorbett1998@hotmail.co.uk>
2019-10-24 21:08:38 +0100

    Init

---
+++ DatabaseTest/index.js
@@ -0,0 +1,49 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request
+var TYPES = require('tedious').TYPES;
+
+module.exports = function (context, myTimer) {
+    var _currentData = {};
+    var config = {
+        server: 'tweeter-data.database.windows.net',
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'jc02rbt',
+                password: 'an1hG8K!55w'
+            }
+        },
+        options: {
+            database: 'tweeter',
+            encrypt: true
+        }
+    };
+    var connection = new Connection(config);
+    connection.on('connect', function(err) {
+        if (err) {
+            context.log(err);}
+        context.log("Connected");
+        getUsername();
+    });
+
+    function getUsername() {
+        request = new Request("select password from users", function(err) {
+            if (err) {
+                context.log(err);}
+        });
+
+        request.on('row', function(columns) {
+            _currentData.Username = columns[0].value;
+            context.log(_currentData);
+        });
+
+        request.on('requestCompleted', function () {
+            context.res = {
+                status: 200,
+                body: "Password: " + (_currentData.Username) 
+            };
+            context.done();
+        });
+        connection.execSql(request);
+    };
+};
